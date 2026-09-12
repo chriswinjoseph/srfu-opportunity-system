5ef855f8dd33dc5dc90a94c106f9839fbcd544d2
Chriswin Joseph <s4054838@student.rmit.edu.au>
2026-09-12 10:25:35 +1000

    Add get_supported_clubs function to outreach/views.py

--- outreach/views.py
+++ outreach/views.py
@@ -1,10 +1,17 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from .models import Bank
+from .models import Bank, Club
 
 
 @login_required
 def bank_list(request):
     banks = Bank.objects.all().order_by("bank_name")
     return render(request, "outreach/bank_list.html", {"banks": banks})
+
+
+def get_supported_clubs(bank):
+    """
+    Given a Bank, return every Club it currently supports.
+    """
+    return Club.objects.filter(supported_by_bank=bank).order_by("club_name")
